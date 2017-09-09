1ac0f2f57ee6528ea6a0ef8d03ff25618100bdbf
Matti Lankinen <m.lankinen@iki.fi>
2017-09-09 21:41:04 +0300

    Downgrade to clojure 1.8 and use “clojure-future-spec” instead of “clojure.spec.alpha”

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
           :source-uri "https://github.com/metosin/spec-tools/{version}/{filepath}#L{line}"
           :metadata {:doc/format :markdown}}
 
-  :dependencies [[org.clojure/spec.alpha "0.1.123"]]
+  :dependencies [[clojure-future-spec "1.9.0-alpha17" :exclusions [org.clojure/clojure]]]
 
   :profiles {:dev {:plugins [[jonase/eastwood "0.2.4"]
                              [lein-tach "0.3.0"]
@@ -21,7 +21,7 @@
                              [lein-codox "0.10.3"]]
                    :jvm-opts ^:replace ["-server"]
                    ;:global-vars {*warn-on-reflection* true}
-                   :dependencies [[org.clojure/clojure "1.9.0-alpha19"]
+                   :dependencies [[org.clojure/clojure "1.8.0"]
                                   [org.clojure/clojurescript "1.9.908"]
                                   [criterium "0.4.4"]
                                   [prismatic/schema "1.1.6"]
